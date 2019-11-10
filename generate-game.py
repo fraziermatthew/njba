05904f490b09bc2a143c49bf29efcdefe96ac1c9
Matthew Frazier <fraziermatthew@hotmail.com>
2019-11-10 08:34:37 -0500

    Delete generate-game.py

--- generate-game.py
+++
@@ -1,7 +0,0 @@
-"""generate-game.py: Generates random NJBA game data."""
-
-__author__ = "Matthew Frazier"
-__copyright__ = "Copyright 2019, University of Delaware, CISC 637 Database Systems"
-__email__ = "matthew@udel.edu"
-
-import csv
